b5063cb5f4a96a234c3e828481b0c32ea280fd44
Amirreza <amirreza.zahraei1@rbe.sk.ca>
2023-08-12 15:39:39 -0600

    the tile.hpp is added and the operators = == and << are overloaded for the class Tile

---
+++ board/tile/tile.cpp
@@ -0,0 +1,26 @@
+//
+// Created by KAVOSH on 8/12/2023.
+//
+
+#include "tile.hpp"
+#include <iostream>
+
+namespace GB {
+    Tile& Tile::operator=(Tile &t) {
+
+        if (&t != this)
+            m_value = t.m_value;
+
+        return *this;
+    }
+
+
+    std::ostream &operator<<(std::ostream &out, Tile &t) {
+        out << t.m_value;
+        return out;
+    }
+
+    bool operator==(Tile &t, int n) {
+        return t.m_value == n;
+    }
+}

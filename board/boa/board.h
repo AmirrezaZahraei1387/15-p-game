b8644dc424fc8b0f4ddffd9fea2a284aefab6ca4
Amirreza <amirreza.zahraei1@rbe.sk.ca>
2023-08-12 15:43:58 -0600

    the info.h is added to project containig the size and the modes of game. also the board.cpp is added for the class Board

--- board/boa/board.h
+++ board/boa/board.h
@@ -4,9 +4,13 @@
 #ifndef INC_15_PUZZLE_GAME_BOARD_H
 #define INC_15_PUZZLE_GAME_BOARD_H
 
+#include "../tile/tile.hpp"
+
 namespace GB{
     class Board{
-
+    public:
+    private:
+        Tile board[][];
     };
 }
 #endif //INC_15_PUZZLE_GAME_BOARD_H
---
+++ info.h
@@ -0,0 +1,14 @@
+//
+// Created by KAVOSH on 8/12/2023.
+//
+#ifndef INC_15_PUZZLE_GAME_INFO_H
+#define INC_15_PUZZLE_GAME_INFO_H
+namespace info{
+    inline constexpr int SIZE{4};
+
+    enum class ModeHap{
+        SUCCESS,
+        FAILURE
+    };
+}
+#endif //INC_15_PUZZLE_GAME_INFO_H

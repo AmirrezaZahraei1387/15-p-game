310397ecccbb3290719f0477911c0df35e587289
Amirreza <amirreza.zahraei1@rbe.sk.ca>
2023-08-12 15:31:08 -0600

    the tile.cpp is removed

--- board/tile/tile.hpp
+++ board/tile/tile.hpp
@@ -4,12 +4,15 @@
 
 #ifndef INC_15_PUZZLE_GAME_TILE_HPP
 #define INC_15_PUZZLE_GAME_TILE_HPP
+#include <iostream>
+
+
 namespace GB{
 
     class Tile{
     public:
         explicit Tile(int value):m_value{value}{}
-
+        friend inline std::ostream& operator<<(std::ostream& out, Tile& t);
     private:
         int m_value{};
     };

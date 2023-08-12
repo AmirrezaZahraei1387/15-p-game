254f47412fe684d6af2eb77414e006319dac30e1
Amirreza <amirreza.zahraei1@rbe.sk.ca>
2023-08-12 15:11:34 -0600

    deleted

--- main.cpp
+++ main.cpp
@@ -1,15 +1,7 @@
-#include <iostream>
-#include "gameBoard/board.hpp"
+
 
 
 int main() {
-    GB::Board board{};
-    board.shuffleTiles();
-    board.prepare();
-    std::cout<<board;
-    GB::PointIndex p{1, 0};
-    board.moveTile(p);
-    std::cout<<std::endl;
-    std::cout<<board;
+
     return 0;
 }

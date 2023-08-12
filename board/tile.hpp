6a9692eaf0834f38097cd0e65a141b7dfbe990fe
Amirreza <amirreza.zahraei1@rbe.sk.ca>
2023-08-12 15:24:12 -0600

    the class Tile is created iside new header file tile.hpp

---
+++ board/tile.hpp
@@ -0,0 +1,12 @@
+//
+// Created by KAVOSH on 8/12/2023.
+//
+
+#ifndef INC_15_PUZZLE_GAME_TILE_HPP
+#define INC_15_PUZZLE_GAME_TILE_HPP
+namespace GB{
+    class Tile{
+
+    };
+}
+#endif //INC_15_PUZZLE_GAME_TILE_HPP

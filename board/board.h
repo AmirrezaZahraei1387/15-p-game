1fc21546a4c7a5c61bc67d00d6b715d4fc92bcf7
Amirreza <amirreza.zahraei1@rbe.sk.ca>
2023-08-12 15:22:21 -0600

    the board.h is added and the class Board is created inside it.

---
+++ board/board.h
@@ -0,0 +1,12 @@
+//
+// Created by KAVOSH on 8/12/2023.
+//
+#ifndef INC_15_PUZZLE_GAME_BOARD_H
+#define INC_15_PUZZLE_GAME_BOARD_H
+
+namespace GB{
+    class Board{
+
+    };
+}
+#endif //INC_15_PUZZLE_GAME_BOARD_H
